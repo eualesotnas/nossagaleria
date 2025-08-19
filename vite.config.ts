22e987f9f506894afd64b0f5ec92edbb18117703
alessandro.santos.es <alessandro.santos.es@gmail.com>
2025-08-19 20:27:23 +0000

    Update- home page, gh pages, run build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -19,4 +20,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/nossagaleria/",
 }));
